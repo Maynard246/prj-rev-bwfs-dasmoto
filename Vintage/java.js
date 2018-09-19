addb3f30bdd41620e72db4ae0bd9e30a0a780a3b
Maynard246 <41711707+Maynard246@users.noreply.github.com>
2018-09-19 18:11:07 +1000

    Add files via upload

--- Vintage/java.js
+++ Vintage/java.js
@@ -8,3 +8,24 @@ $(document).ready(function(){
     }
  })
  })
+ $(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100){
+            $(".fade-in").css({"opacity" : "0"})
+        }
+ else {
+        $(".fade-in").css({"opacity" : "1"})
+    }
+ })
+ })
+ 
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300){
+            $(".history").css({"opacity" : "1"})
+        }
+ else {
+        $(".history").css({"opacity" : "0"})
+    }
+ })
+ })
